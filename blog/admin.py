6e03e678f2f32b7fd417f810c5ffa98ed33bb7f5
Abhinav <abhiasthana76@gmail.com>
2026-03-02 19:20:36 +0530

    feat(admin): :sparkles: register model on admin

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from markdownx.admin import MarkdownxModelAdmin
+from .models import Post
 
-# Register your models here.
+
+@admin.register(Post)
+class PostAdmin(MarkdownxModelAdmin):
+    list_display = ("title", "published_date", "status")
+    list_filter = ("status", "published_date")
+    search_fields = ("title", "content")
+    prepopulated_fields = {"slug": ("title",)}
