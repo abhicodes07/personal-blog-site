2123d8dba434a370d68de06512a32460e0ce9816
Abhinav <abhiasthana76@gmail.com>
2026-03-03 18:16:15 +0530

    chore(views): :hammer: use function based views instead of class based views

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,26 @@
-from django.shortcuts import render
-from django.views.generic import ListView
+from django.shortcuts import get_object_or_404, render
+from django.views.generic import ListView, DetailView
 from .models import Post
 
 # Create your views here.
 
 
-class HomeView(ListView):
-    model = Post
-    template_name = "blog/home.html"
-    context_object_name = "recent_posts"
-    queryset = Post.objects.filter(status="published")[:5]
+def home(request):
+    recent_posts = Post.objects.filter(status="published").order_by("-published_date")[
+        :5
+    ]
+    featured_post = Post.objects.filter(status="published", featured=True).first()
+    context = {"recent_posts": recent_posts, "featured_post": featured_post}
+    return render(request, "blog/home.html", context=context)
+
+
+def post_list(request):
+    all_posts = Post.objects.filter(status="published")
+    context = {"all_posts": all_posts}
+    return render(request, "blog/post_list.html", context=context)
+
+
+def post_detail(request, slug):
+    post = get_object_or_404(Post, slug=slug, status="published")
+    context = {"post": post}
+    return render(request, "blog/post_detail.html", context=context)
