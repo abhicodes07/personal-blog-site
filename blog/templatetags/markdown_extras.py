3355be6e4f435b3dee211e03f45267675b3df908
Abhinav <abhiasthana76@gmail.com>
2026-03-05 11:42:39 +0530

    fix(templatetag): :bug: fix markdown table rendering

--- blog/templatetags/markdown_extras.py
+++ blog/templatetags/markdown_extras.py
@@ -1,3 +1,4 @@
+import re
 from django import template
 from markdownx.utils import markdownify
 from django.utils.safestring import mark_safe
@@ -10,4 +11,11 @@ def render_markdown(text):
     """Renders Markdownx content with syntax-ready code blocks"""
     if not text:
         return ""
-    return mark_safe(markdownify(text))
+
+    html = markdownify(text)
+
+    # Wrap tables in a responsive scroll container
+    html = re.sub(r'(<table)', r'<div class="table-wrapper">\1', html)
+    html = re.sub(r'(</table>)', r'\1</div>', html)
+
+    return mark_safe(html)
